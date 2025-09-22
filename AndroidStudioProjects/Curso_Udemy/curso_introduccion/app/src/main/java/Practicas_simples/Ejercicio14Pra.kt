480869f81025f6232fe0f2b8d14ca703bb98cf4d
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-09-22 22:20:21 +0200

    Ejercicios básicos

--- AndroidStudioProjects/Curso_Udemy/curso_introduccion/app/src/main/java/Practicas_simples/Ejercicio14Pra.kt
+++ AndroidStudioProjects/Curso_Udemy/curso_introduccion/app/src/main/java/Practicas_simples/Ejercicio14Pra.kt
@@ -3,4 +3,7 @@ package Practicas_simples
 fun main() {
     var nombre: String? = readLine()
     println(nombre?.uppercase() ?: "Error")
+
+    var num: Double? = readLine()?.toDoubleOrNull()
+    println(num ?: "Error")
 }
