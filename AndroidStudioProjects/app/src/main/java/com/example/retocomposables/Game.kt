d7e4b9c1507654bb2a7991631ad489b49001bae2
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2025-11-02 01:35:47 +0100

    Juego del super contador, practica terminada

--- AndroidStudioProjects/app/src/main/java/com/example/retocomposables/Game.kt
+++
@@ -1,7 +0,0 @@
-package com.example.retocomposables
-
-// Creamos una data class que va a guardar información básica importante
-data class Game(
-    val nombre: String = "Invitado",
-    var score: Int = 0,
-    var level: Int = 0)
